402f4f5eef56cc34dcb6042a3d15decc8a77fff8
Sandeepme_ <sandeepprajapati9158@gmail.com>
2025-08-31 02:46:30 +0530

    add ignore file and CORS

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.routers import router as task_router
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -8,5 +8,11 @@ app = FastAPI()
 def start():
     return {"message":"helo"}
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(task_router)
